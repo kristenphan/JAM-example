e0a0ba531fd759e333987397d1778e138a741a2b
Kristen Phan <hgiang.phan@yahoo.com.vn>
2022-08-31 17:40:29 +0200

    added envi variables in .env

--- ex4/src/_data/news.js
+++ ex4/src/_data/news.js
@@ -1,13 +1,11 @@
-// const API-KEY = "b727690f2cec409cbf590220b63dc2ef";
-// `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API-KEY}&pageSize=5`
-
 const axios = require("axios");
+require('dotenv').config();
 
 module.exports = async function() {
   try {
-    const response = await axios.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=b727690f2cec409cbf590220b63dc2ef");
+    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`);
     return response.data;
   } catch (error) {
     console.error(error);
   }
-}
+};
